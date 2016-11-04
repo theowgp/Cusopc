972218212ba29b6310f52997910be4ed08649202
myroslav <theowgp@gmail.com>
2016-11-04 13:40:08 +0100

    changed the potential V, fixed the problem with small initial data

--- control.m
+++ control.m
@@ -5,13 +5,15 @@ temp = zeros(1, d);
 for j = 1:N
     if A(i, j)
 %         temp = temp-    dV(norm(x(i, :) - x(j, :)), R) * (x(i, :) - x(j, :)) / norm(x(i, :) - x(j, :));
+
+        temp = temp+    2 * V(norm(x(i, :) - x(j, :)), R) * (v(i, :) - v(j, :));
         
-        rij = norm(x(i, :) - x(j, :));
-        if rij >  R/2
-            temp = temp+    -2 * V(rij, R) * (v(i, :) - v(j, :));
-        else
-            temp = temp+     2 * V(rij, R) * (v(i, :) - v(j, :));
-        end
+%         rij = norm(x(i, :) - x(j, :));
+%         if rij >  R/2
+%             temp = temp+    -2 * V(rij, R) * (v(i, :) - v(j, :));
+%         else
+%             temp = temp+     2 * V(rij, R) * (v(i, :) - v(j, :));
+%         end
     end
 end
 
--- main.m
+++ main.m
@@ -1,3 +1,4 @@
+clear all
 
 %% PARAMETERS:
 % number of agents
